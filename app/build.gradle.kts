de10dd76e7486d4b0d249a892f9a810935480d38
Nils Druyen <info@nilsdruyen.de>
2020-08-15 18:53:54 +0200

    chore: update as, kotlin & app gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ repositories {
 val daggerVersion: String by project
 dependencies {
   // Kotlin
-  implementation(kotlin("stdlib-jdk8"))
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.8-1.4.0-rc")
 
   // AndroidX
@@ -64,7 +63,7 @@ dependencies {
   implementation("androidx.fragment:fragment:1.3.0-alpha07")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha06")
   implementation("androidx.recyclerview:recyclerview:1.2.0-alpha05")
-  implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta8")
+  implementation("androidx.constraintlayout:constraintlayout:2.0.0-rc1")
 
   // UI
   implementation("com.google.android.material:material:1.3.0-alpha02")
