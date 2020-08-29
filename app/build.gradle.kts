683951e88bfed185386629bb5f166629a05ecd90
Stefan Neidig <stefan@rpdev.net>
2020-08-29 15:43:54 +0200

    Update kotlin version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   kotlin("kapt")
   id("dagger.hilt.android.plugin")
   id("com.huawei.agconnect")
+  id("kotlin-android")
 }
 
 android {
@@ -79,6 +80,7 @@ dependencies {
 
   // Web
   implementation("com.squareup.moshi:moshi:1.9.3")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra["kotlin_version"]}")
   kapt("com.squareup.moshi:moshi-kotlin-codegen:1.9.3")
   implementation("com.squareup.okhttp3:okhttp:4.8.1")
   implementation("com.squareup.okhttp3:logging-interceptor:4.8.1")
