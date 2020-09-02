d1ca912bcbdec39104569d26913c22ed47691f5d
Nils Druyen <n.druyen@nanogiants.de>
2020-09-02 20:43:55 +0200

    feat: add mapview

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     maven("http://developer.huawei.com/repo/")
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:${properties["androidGradlePluginVersion"]}")
+    classpath("com.android.tools.build:gradle:4.1.0-rc02")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${properties["kotlinVersion"]}")
     classpath("com.google.dagger:hilt-android-gradle-plugin:${properties["daggerVersion"]}")
     classpath("com.huawei.agconnect:agcp:${properties["agcpVersion"]}")
