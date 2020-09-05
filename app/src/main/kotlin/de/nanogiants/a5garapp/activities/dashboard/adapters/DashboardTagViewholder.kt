559560b9628d2819f6493eb9619a6c6e0448a288
Stefan Neidig <stefan@rpdev.net>
2020-09-05 21:02:32 +0200

    Add icons to tags in dashboard recycler view

--- app/src/main/kotlin/de/nanogiants/a5garapp/activities/dashboard/adapters/DashboardTagViewholder.kt
+++ app/src/main/kotlin/de/nanogiants/a5garapp/activities/dashboard/adapters/DashboardTagViewholder.kt
@@ -5,6 +5,7 @@
 
 package de.nanogiants.a5garapp.activities.dashboard.adapters
 
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import de.nanogiants.a5garapp.databinding.ItemDashboardPoiBinding
@@ -20,5 +21,55 @@ class DashboardTagViewholder(val viewBinding: ItemTagBinding) :
   fun bind(item: SelectableTag) {
     viewBinding.chipTagTextView.text = item.name.capitalize(Locale.getDefault())
     viewBinding.chipTagTextView.isSelected = item.selected
+    viewBinding.chipRootView.isSelected = item.selected
+
+    val context = viewBinding.tagIconImageView.context
+    val id = context.resources.getIdentifier(
+      getDrawableNameForTag(item.id, item.selected),
+      "drawable",
+      context.packageName
+    )
+    viewBinding.tagIconImageView.setImageDrawable(ContextCompat.getDrawable(context, id))
+  }
+
+  private fun getDrawableNameForTag(tagId: Int, selected: Boolean): String {
+    val name = when (tagId) {
+      1 -> "ic_coffee" // restaurant
+
+      2 -> "ic_map_outline" // place
+
+      3 -> "ic_image_outline" // museum
+
+      4 -> "ic_color_palette_outline" // art
+
+      5 -> "ic_speaker" // concert
+
+      6 -> "ic_flag" // kiosk
+
+      7 -> "ic_flag" // bar
+
+      8 -> "ic_flag" // zoo
+
+      9 -> "ic_music_outline" // music
+
+      10 -> "ic_anchor" // water
+
+      11 -> "ic_film_outline" // theater
+
+      12 -> "ic_resource_package" // startup
+
+      13 -> "ic_briefcase_outline" // business
+
+      14 -> "ic_flag" // lookout
+
+      15 -> "ic_home_outline__1_" // architecture
+
+      16 -> "ic_flag" // hotel
+
+      17 -> "ic_flag" // historic
+      else -> ""
+    }
+
+    return "$name${if (selected) "_selected" else ""}"
   }
 }
